f534b3df724ca9f50882c6cd4993ae787289c00d
Mark <mtik.philosopher@gmail.com>
2025-08-22 16:35:06 -0700

    submission week1

--- week1_exercise1.cpp
+++ week1_exercise1.cpp
@@ -1,14 +1,12 @@
-//STARTER 
-//THIS EXERCISE IS TO BE DONE IN THE ARM MBED SIMULATOR
-//http://195.130.59.221/
-//IF YOU HAVE ISSUES WITH THE SIMULATOR, CONTACT EMBEDDED LEAD
 #include "mbed.h"
 
-int main() {
-    while (1) {
-        printf("Week 1 Exercise 1");
+DigitalOut led(LED1);
+DigitalIn button(BUTTON1);
 
-        // MAKE SURE THERE IS ALWAYS A WAIT ON THE SIM OR IT WILL CRASH
-        wait_ms(500); 
+int main() {
+    printf("Week 1 Exercise 1");
+    while (1) {
+        led.write(button.read());
+        wait_ms(500);
     }
 }
--- week1_exercise2.cpp
+++ week1_exercise2.cpp
@@ -3,12 +3,24 @@
 //http://195.130.59.221/
 //IF YOU HAVE ISSUES WITH THE SIMULATOR, CONTACT EMBEDDED LEAD
 #include "mbed.h"
+#include <cstdint>
+DigitalOut led(LED1);
+AnalogIn potentiometer(A0); // nice that the web version says p15 lol
 
+const uint32_t flick_period = 2000; //ms
+ 
 int main() {
+    printf("Week 1 Exercise 2");
     while (1) {
-        printf("Week 1 Exercise 2");
-
-        // MAKE SURE THERE IS ALWAYS A WAIT ON THE SIM OR IT WILL CRASH
-        wait_ms(500); 
+        float reading = potentiometer.read();
+        uint32_t on_time = static_cast<uint32_t>(
+            reading * flick_period
+        );
+        
+        uint32_t off_time = flick_period - on_time;
+        led.write(true);
+        wait_ms(on_time);
+        led.write(false);
+        wait_ms(off_time);
     }
 }
